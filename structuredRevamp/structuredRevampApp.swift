5d520698b29e7c62e41c5086e74793826e5fdb0e
zPokek98 <iglithcraft@gmail.com>
2023-11-14 17:15:14 +0100

    Initial Commit

---
+++ structuredRevamp/structuredRevampApp.swift
@@ -0,0 +1,17 @@
+//
+//  structuredRevampApp.swift
+//  structuredRevamp
+//
+//  Created by Salvo on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct structuredRevampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
